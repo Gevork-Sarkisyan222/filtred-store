94ee1e3f14e76810c89ef465e767602bac5890ac
Gevork-Sarkisyan222 <zadroterkom@gmail.com>
2025-01-23 21:31:08 +0400

    provide component into suspence

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,16 @@ import FilterOptions from '@/components/filter-options';
 import Header from '@/components/header';
 import HomeTemplate from '@/templates/home-template';
 import axios from 'axios';
+import { Suspense } from 'react';
 
 interface ProductsResponse {
   products: CardProps[];
 }
 
 export default async function Home() {
-  return <HomeTemplate />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomeTemplate />
+    </Suspense>
+  );
 }
