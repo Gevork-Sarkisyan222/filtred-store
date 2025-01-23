f009687f94e1e7af87c209015166f90a6a0c1549
Gevork-Sarkisyan222 <zadroterkom@gmail.com>
2025-01-23 21:19:55 +0400

    fix string types

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,13 +1,7 @@
 import ProductPageTemplate from '@/templates/product-page-template';
 import React from 'react';
 
-type Props = {
-  params: { id: string };
-};
-
-function ProductPage({ params }: Props) {
-  const { id } = params;
-
+function ProductPage({ params: { id } }: { params: { id: string } }) {
   return <ProductPageTemplate productId={id} />;
 }
 
